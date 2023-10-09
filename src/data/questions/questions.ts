56c2fefa5241c533908cefc2fa89702637021bbf
David92p <Dave.panetta92@gmail.com>
2023-10-09 20:17:49 +0200

    Created draft possible questions

---
+++ src/data/questions/questions.ts
@@ -0,0 +1,142 @@
+import { Question } from "../../datatypes";
+
+export const QUESTIONS_ITA: Question[] = [
+  {
+    question:
+      "Qual'è il primo responsabile di produzione di anidride carbonica nel nostro pianeta?",
+    options: [
+      "Irragazione con macchinari agricoli",
+      "Allevamenti intensivi",
+      "Deforestazione",
+      "Installazioni piattaforme artificiali sui mari",
+    ],
+    answer: 1,
+  },
+  {
+    question: "Qual'è la prima causa di deforestazione sul pianeta?",
+    options: [
+      "Commercio di carne",
+      "Produzione di legna destinato alle abitazioni",
+      "Utilizzo delle acque per la produzione di energia elettrica",
+      "Utilizzo di prodotti chimici dannosi per l'atmosfera",
+    ],
+    answer: 0,
+  },
+  {
+    question:
+      "Quale tra queste diete è secondo te la meno impattante sulla produzione di gas serra?",
+    options: [
+      "Dieta vegetariana",
+      "Dieta onnivora",
+      "Dieta pescetariana",
+      "Dieta vegana",
+    ],
+    answer: 3,
+  },
+  {
+    question: "Quali frutta ha un minor impatto ambiantale?",
+    options: [
+      "Frutta esotica",
+      "Frutta di stagione",
+      "Frutta conservata tramite congelamento",
+      "Frutta a lenta maturazione",
+    ],
+    answer: 1,
+  },
+  {
+    question:
+      "Quale imballaggio preferire per conservare prodotti come frutta e verdura?",
+    options: [
+      "Imballaggio di carta",
+      "Imballaggio biodegradabile",
+      "Frutta e verdura sfusa senza imballaggio",
+      "Grossi imballaggi come cassette in legno, per una maggiore conservazione",
+    ],
+    answer: 2,
+  },
+  {
+    question:
+      "I surrogati della carne come i burger vegetali che hanno la pretesa di inserirsi come sostituti della carne per colore, forma e consistenza, questi hanno un impatto sostenibile?",
+    options: [
+      "Si, la produzione di questi prodotti non hanno impatto sul nostro pianeta",
+      "No, la produzione di questi alimenti hanno un impatto superiore rispetto al consumo di carni bianche",
+      "No, vi sono all'interno oli raffinali parzialmente idrogenati, amido modificato, aromi chimici e glicerina che aiutano la consistenza a essere il più vicina possibile a quella della carne ",
+      "Si, la produzione di questi alimenti permette di non utilizzare le risorse ambientali",
+    ],
+    answer: 2,
+  },
+  {
+    question:
+      "Qual'è la bevanda a base di latte con il minor impatto ambientale?",
+    options: [
+      "Latte scremato",
+      "Latte intero",
+      "Latte di mandorla",
+      "Latte d'avena",
+    ],
+    answer: 3,
+  },
+  {
+    question:
+      "Quale tecnica consente di pescare in maniera responsabile e attenta alla sopravvivenza della fauna marina?",
+    options: [
+      "La pesca con lenza",
+      "La pesca con la rete",
+      "Lo pesca a strascico",
+      "La pesca con allevamento selezionato",
+    ],
+    answer: 0,
+  },
+  {
+    question:
+      "Quale tecnica di agricoltura risulta sostenibile per il nostro pianeta?",
+    options: [
+      "L'agricoltura con l'utilizzo di diserbanti e pesticidi",
+      "L'agricolutra attraverso l'utilizzo di serre selezionate",
+      "L'agricoltura con impianti solari per favorire la produzione",
+      "L'agricoltura biologia e biodinamica",
+    ],
+    answer: 3,
+  },
+  {
+    question:
+      "Quale imballaggio ad oggi risulta sostenibile per il nostro pianeta?",
+    options: ["Vetro", "Bioplastica", "Legno", "Carta"],
+    answer: 1,
+  },
+  {
+    question:
+      "Quale imballaggio è consigliato per conservare i nostri alimenti a casa?",
+    options: [
+      "Barattoli e contenitori di vetro",
+      "Scatola di cartone",
+      "Buste di plastica con zip salvafreschezza",
+      "Alluminio",
+    ],
+    answer: 0,
+  },
+  {
+    question: "Quale pratica di coltura favorisce la conservazione del suolo?",
+    options: [
+      "Monocoltura",
+      "Rotazione delle colture",
+      "Irrigazione eccessiva",
+      "Uso di pesticidi chimici",
+    ],
+    answer: 1,
+  },
+];
+
+export const QUESTIONS_ENG: Question[] = [
+  {
+    question:
+      "What is primarily responsible for the production of carbon dioxide on our planet?",
+    options: [
+      "Irrigation with agricultural machinery",
+      "Intensive farming",
+      "Deforestation",
+      "Installations of artificial platforms on the seas",
+    ],
+    answer: 1,
+  },
+];
---
+++ src/datatypes/questions/questions.ts
@@ -0,0 +1,7 @@
+interface Question {
+  question: string;
+  options: [string, string, string, string];
+  answer: 0 | 1 | 2 | 3;
+}
+
+export default Question;
