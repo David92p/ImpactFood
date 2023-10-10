4753bbd6c237ca68abbcc77ec705038a12fb8eca
David92p <dave.panetta92@gmail.com>
2023-10-10 17:49:23 +0200

    Fixed bg

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Starter } from "./components";
 
 const App = () => {
   return (
-    <div className="flex justify-center min-h-screen bg-slate-800 py-10">
+    <div className="flex justify-center min-h-screen bg-slate-900 py-10">
       <Starter />
     </div>
   );
