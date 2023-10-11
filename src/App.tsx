fc3cb97cc027538b740a3ba6f1a97180987eb876
David92p <Dave.panetta92@gmail.com>
2023-10-11 19:42:58 +0200

    Fixed App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import { Starter } from "./components";
 
 const App = () => {
   return (
-    <div className="flex justify-center min-h-screen bg-slate-900 py-10">
-      <Starter />
-    </div>
+    <Starter />
   );
 };
 
