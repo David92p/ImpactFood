946239de4fa692e4fb0899e2bc733f468db33697
David92p <Dave.panetta92@gmail.com>
2023-10-12 20:19:12 +0200

    Fixed context and rendering state

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,33 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import en from "../../assets/images/en.png";
 import it from "../../assets/images/it.png";
 import { BsSun, BsMoon } from "react-icons/bs";
-import { AppContext } from '../../context/context';
+import { DispatchContext, StarterContext } from "../../context/context";
 
-const Header:React.FC = () => {
+const Header: React.FC = () => {
+  const context = useContext(StarterContext);
+  const dispatch = useContext(DispatchContext)
 
-  const context = useContext(AppContext)
-  console.log(context);
-  
-  //context.dispatch({type: "darkMode"})
-	return (
+  return (
     <div className="flex justify-between px-4 gap-10 w-[100%]">
       <div className="flex gap-4">
-				<img src={en} alt="english" className="w-8 h-8 sm:w-10 sm:h-10 rounded-full cursor-pointer" />
-        <img src={it} alt="italian" className="w-8 h-8 sm:w-10 sm:h-10 rounded-full cursor-pointer" />
+        <img
+          src={en}
+          alt="english"
+          className="w-8 h-8 sm:w-10 sm:h-10 rounded-full cursor-pointer"
+        />
+        <img
+          src={it}
+          alt="italian"
+          className="w-8 h-8 sm:w-10 sm:h-10 rounded-full cursor-pointer"
+        />
       </div>
-      <label className="bg-slate-300 relative w-16 h-8 rounded-full">
+      <label
+        className={`${
+          context?.darkMode ? "bg-green-200" : "bg-green-400"
+        } relative w-16 h-8 rounded-full`}
+      >
         <input
-          onClick={() => context.dispatch({ type: "darkMode" })}
+          onClick={() => dispatch({ type: "darkMode" })}
           type="checkbox"
           className="sr-only peer "
         />
         <span className="flex justify-center items-center w-2/5 h-4/5 bg-slate-900 absolute rounded-full left-1 top-1 cursor-pointer peer-checked:bg-slate-200 peer-checked:left-8 transition-all duration-500">
-          {context.state.darkMode ? <BsMoon /> : <BsSun color={"black"} />} 
+          {context?.darkMode ? <BsMoon /> : <BsSun color={"green"} />}
         </span>
       </label>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,62 +1,85 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 
 import logo from "../../assets/images/logo.jpg";
 
-import { AppContext } from '../../context/context';
+import { DispatchContext, StarterContext } from "../../context/context";
 
-const Home:React.FC = () => {
-
-  const context = useContext(AppContext)
+const Home: React.FC = () => {
+  const context = useContext(StarterContext);
+  const dispatch = useContext(DispatchContext)
+  console.log(context);
+  
   return (
     <>
-      <div className={`text-3xl sm:text-4xl text-center font-medium mt-2 ${context.state.darkMode ? null : "font-extrabold"}`}>
-          Benvenuto su ImpactFood
-        </div>
-        <img src={logo} alt="logo" className="w-auto sm:w-1/2 mt-2 px-6" />
-        <div className="flex flex-col items-center my-4 text-xl sm:text-lg text-center">
-          Per iniziare il test, clicca sul pulsante qui sotto.
-          <button className="btn my-auto mt-2 text-xl ">START</button>
-        </div>
-        <div className="flex flex-col w-[100%] h-full text-lg sm:text-2xl">
-          {context.state.documentation ? (
-            <div className="w-[100%] h-auto">
-              Contenuto testo bottone{" "}
+      <div className="text-3xl sm:text-4xl text-center mt-8 font-extrabold">
+        Benvenuto su ImpactFood
+      </div>
+      <img src={logo} alt="logo" className="w-auto sm:w-1/2 mt-2 px-6" />
+      <div className="flex flex-col items-center my-4 text-xl sm:text-lg text-center font-bold">
+        Per iniziare il test, clicca sul pulsante qui sotto.
+        <button
+          className={`px-4 py-2 w-24 rounded-xl tracking-wider my-auto mt-2 text-xl text-slate-900 hover:font-extrabold ${
+            context?.darkMode
+              ? " bg-green-200 hover:bg-green-300"
+              : " bg-green-400 hover:bg-green-500"
+          }`}
+        >
+          START
+        </button>
+      </div>
+      <div className="flex flex-col w-[100%] h-full text-lg sm:text-2xl">
+        {context?.toggleDocumentation ? (
+         <div className="flex justify-center" >
+            <div className="w-full sm:w-2/3 h-24 bg-white" onClick={() => dispatch({ type: "documentation" })}>{ context?.documentation }</div>
+         </div>
+        ) : (
+          <>
+            <div className="flex justify-center h-1/2 sm:h-1/3 gap-x-3 mb-2 text-slate-900 font-extrabold">
+              <button
+              onClick={() => dispatch({ type: "documentation" })}
+                className={`w-1/2 sm:w-1/3 h-20 border-2 rounded-xl ${
+                  context?.darkMode
+                    ? "bg-green-200 hover:bg-green-300"
+                    : "bg-green-300 hover:bg-green-400"
+                }`}
+              >
+                Supporto
+              </button>
+              <button
+                className={`w-1/2 sm:w-1/3 h-20 border-2 rounded-xl ${
+                  context?.darkMode
+                    ? "bg-green-200 hover:bg-green-300"
+                    : "bg-green-300 hover:bg-green-400"
+                }`}
+              >
+                Calcolo punteggio
+              </button>
             </div>
-          ) : (
-            <>
-              <div className="flex justify-center h-1/2 sm:h-1/3 gap-x-3 mb-2">
-                <button
-                  className="w-1/2 sm:w-1/3 h-20 border-2 rounded-xl hover:bg-slate-200 hover:text-slate-800"
-                  
-                >
-                  Supporto
-                </button>
-                <button
-                  className="w-1/2 sm:w-1/3 h-20 border-2 rounded-xl hover:bg-slate-200 hover:text-slate-800"
-                  
-                >
-                  Calcolo punteggio
-                </button>
-              </div>
-              <div className="flex justify-center h-1/2 sm:h-1/3 gap-x-3 ">
-                <button
-                  className="w-1/2 sm:w-1/3 h-20 border-2 rounded-xl hover:bg-slate-200 hover:text-slate-800"
-                 
-                >
-                  Perchè farlo?
-                </button>
-                <button
-                  className="w-1/2 sm:w-1/3 h-20 border-2 rounded-xl hover:bg-slate-200 hover:text-slate-800"
-                  
-                >
-                  Perchè è utile?
-                </button>
-              </div>
-            </>
-          )}
-        </div>
-      </>
-  )
-}
+            <div className="flex justify-center h-1/2 sm:h-1/3 gap-x-3 text-slate-900 font-extrabold">
+              <button
+                className={`w-1/2 sm:w-1/3 h-20 border-2 rounded-xl ${
+                  context?.darkMode
+                    ? "bg-green-200 hover:bg-green-300"
+                    : "bg-green-300 hover:bg-green-400"
+                }`}
+              >
+                Perchè farlo?
+              </button>
+              <button
+                className={`w-1/2 sm:w-1/3 h-20 border-2 rounded-xl ${
+                  context?.darkMode
+                    ? "bg-green-200 hover:bg-green-300"
+                    : "bg-green-300 hover:bg-green-400"
+                }`}
+              >
+                Perchè è utile?
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
