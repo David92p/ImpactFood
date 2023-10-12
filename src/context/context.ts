abab43210d8d36b00d5eae9bf16c3537e6d82e1b
David92p <Dave.panetta92@gmail.com>
2023-10-12 20:18:03 +0200

    Created type and fixed context

--- src/context/context.ts
+++ src/context/context.ts
@@ -1,21 +1,37 @@
 import { createContext } from "react";
 import { GlobalState } from "../datatypes";
 
-export const reducer = (state, action) => {
+// const ACTIONS = {
+//   DARK_MODE: "darkMode",
+//   DOCUMENTATION: "documentation"
+// }
+
+type ActionType = "language" | "darkMode" | "documentation";
+type Action = { type: ActionType, payload?: GlobalState}
+type Dispatch = (action: Action) => void
+
+export const StarterContext = createContext<GlobalState | undefined>(undefined)
+export const DispatchContext = createContext<Dispatch>(() => null)
+
+export const starterReducer = (state:GlobalState, action:Action) => {
   switch (action.type) {
     case "darkMode":
       return { ...state, darkMode: !state.darkMode };
+    case "documentation":
+      return { ...state, toggleDocumentation: !state.toggleDocumentation }
     default:
       return state
   }
 };
 
-export const initialState:GlobalState = {
+export const initialState:GlobalState= {
   language: true,
   darkMode: true,
-  documentation: false
+  toggleDocumentation: false,
+  documentation: "contenuto inserito qui",
+  questionnaire: false
 };
 
-export const AppContext = createContext(initialState)
+
 
 
