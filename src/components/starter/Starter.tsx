9ed26137152c9bd83ccadc6349dd5f211afce676
David92p <Dave.panetta92@gmail.com>
2023-10-12 20:18:37 +0200

    Added context dispatch

--- src/components/starter/Starter.tsx
+++ src/components/starter/Starter.tsx
@@ -1,21 +1,29 @@
 import React, { useReducer } from "react";
-import { Header, Home, Questonary } from "..";
-import { AppContext, initialState, reducer } from "../../context/context";
+import { Header, Home, Questionnaire } from "..";
+import { StarterContext, DispatchContext, initialState, starterReducer } from "../../context/context";
 
-
-const Starter:React.FC = () => {
-  const [state, dispatch] = useReducer(reducer, initialState)
+const Starter: React.FC = () => {
+  const [state, dispatch] = useReducer(starterReducer, initialState);
 
   return (
-    <AppContext.Provider value={{state, dispatch: dispatch}}>
-      <div className={`flex justify-center min-h-screen ${state.darkMode ? "bg-slate-900" : "bg-gray-300"} py-10`}>
-        <div  className={`flex flex-col items-center ${state.darkMode ? "text-white" : "text-black font-extrabold"} sm:w-[50%] mx-4 tracking-wider`}>
-          <Header/>
-          { state.questonary ? <Questonary/> : <Home/> }
+    <StarterContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        <div
+          className={`flex justify-center min-h-screen ${
+            state.darkMode ? "bg-slate-900" : "bg-green-100"
+          } py-10`}
+        >
+          <div
+            className={`flex flex-col items-center ${
+              state.darkMode ? "text-slate-300" : "text-slate-900"
+            } sm:w-[50%] mx-4 tracking-wider"`}
+          >
+            <Header />
+            {state.questionnaire ? <Questionnaire/> : <Home />}
+          </div>
         </div>
-      </div>
-      
-    </AppContext.Provider>
+      </DispatchContext.Provider>
+    </StarterContext.Provider>
   );
 };
 
