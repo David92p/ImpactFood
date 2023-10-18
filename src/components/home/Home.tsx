4bc10b6281f705dfb1ecc2e2cf4bbbcf076a0d37
David92p <Dave.panetta92@gmail.com>
2023-10-18 21:35:40 +0200

    Added new context and created local state comp

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,26 +1,28 @@
-import React, { useContext } from "react";
-
+import React, { useContext, useEffect, useState } from "react";
 import logo from "../../assets/images/logo.jpg";
 import { IoIosArrowBack } from "react-icons/io";
-import { DispatchContext, StarterContext } from "../../context/context";
+import { StateHome } from "../../datatypes";
+import { CONTEXT, DISPATCH } from "../../context";
+import { HOME_ENG, HOME_ITA } from "../../data";
 
 const Home: React.FC = () => {
-  const context = useContext(StarterContext);
-  const dispatch = useContext(DispatchContext);
+  const context = useContext(CONTEXT);
+  const dispatch = useContext(DISPATCH);
+
+  const [stateHome, setStateHome] = useState<StateHome>(HOME_ENG);
+
+  useEffect(() => {
+    context.language == "ENG" ? setStateHome(HOME_ENG) : setStateHome(HOME_ITA);
+  }, [context.language]);
 
   return (
     <>
       <div className="text-3xl sm:text-4xl text-center mt-8 font-extrabold">
-        {context?.language == "ENG"
-          ? "Welcome to ImpactFood"
-          : "Benvenuto su ImpactFood"}
+        {stateHome.title}
       </div>
       <img src={logo} alt="logo" className="w-auto sm:w-1/2 mt-2 px-6" />
-      <div className="flex flex-col items-center my-4 text-xl sm:text-lg text-center font-bold">
-        {context?.language == "ENG"
-          ? "To start the test, click on the button below."
-          : "Per iniziare il test, clicca sul pulsante qui sotto."}
-
+      <div className="flex flex-col items-center my-4 mx-2 text-xl sm:text-lg text-center font-bold">
+        {stateHome.subtitle}
         <button
           onClick={() => dispatch({ type: "toggleQuestionnaire" })}
           className={`px-4 py-2 w-24 rounded-xl tracking-wider my-auto mt-2 text-xl text-slate-900 hover:font-extrabold ${
@@ -33,22 +35,29 @@ const Home: React.FC = () => {
         </button>
       </div>
       <div
-        className={`flex flex-col ${
-          context?.toggleDocumentation ? "items-center" : null
-        } w-[100%] h-auto sm:gap-y-10 text-lg sm:text-2xl`}
+        className={`flex flex-col  w-[100%] h-auto sm:gap-y-10 text-lg sm:text-2xl p-4 ${
+          stateHome.toggleBtnDocumentation ? "items-center" : null
+        }`}
       >
-        {context?.toggleDocumentation ? (
+        {stateHome.toggleBtnDocumentation ? (
           <div className="text-justify text-black p-4 sm:w-2/3 bg-slate-100 rounded-xl">
-            {context.documentation}
+            {stateHome.documentation.description}
             <button
-              onClick={() => dispatch({ type: "toggleDoc" })}
+              onClick={() =>
+                setStateHome((prev: StateHome) => {
+                  return {
+                    ...prev,
+                    toggleBtnDocumentation: !prev.toggleBtnDocumentation,
+                  };
+                })
+              }
               className={`flex items-center px-4 py-2 w-auto rounded-xl tracking-wider my-auto mt-2 text-xl text-slate-900 hover:font-extrabold  ${
                 context?.darkMode
                   ? " bg-green-200 hover:bg-green-300"
                   : " bg-green-400 hover:bg-green-500"
               }`}
             >
-              {context.language == "ENG" ? "Back" : "Indietro"}{" "}
+              { context.language === "ENG" ? "BACK" : "TORNA" }
               <IoIosArrowBack />
             </button>
           </div>
@@ -56,18 +65,17 @@ const Home: React.FC = () => {
           <>
             <div className="flex justify-center h-1/2 sm:h-1/3 gap-x-3 mb-2 text-slate-900 font-extrabold">
               <button
-                onClick={() => dispatch({ type: "toggleDoc", payload: "help" })}
-                className={`w-1/2 sm:w-1/3 h-20 border-2 rounded-xl ${
-                  context?.darkMode
-                    ? "bg-green-200 hover:bg-green-300"
-                    : "bg-green-300 hover:bg-green-400"
-                }`}
-              >
-                {context?.language == "ENG" ? "Help" : "Supporto"}
-              </button>
-              <button
                 onClick={() =>
-                  dispatch({ type: "toggleDoc", payload: "calculation" })
+                  setStateHome((prev: StateHome) => {
+                    return {
+                      ...prev,
+                      toggleBtnDocumentation: !prev.toggleBtnDocumentation,
+                      documentation: {
+                        options: prev.documentation.options,
+                        description: stateHome.documentation.options[0],
+                      },
+                    };
+                  })
                 }
                 className={`w-1/2 sm:w-1/3 h-20 border-2 rounded-xl ${
                   context?.darkMode
@@ -75,25 +83,43 @@ const Home: React.FC = () => {
                     : "bg-green-300 hover:bg-green-400"
                 }`}
               >
-                {context?.language == "ENG"
-                  ? "Score calculation"
-                  : "Calcolo punteggio"}
+                {stateHome.btnDocumentation[0]}
+              </button>
+              <button
+                onClick={() =>
+                  setStateHome((prev: StateHome) => {
+                    return {
+                      ...prev,
+                      toggleBtnDocumentation: !prev.toggleBtnDocumentation,
+                      documentation: {
+                        options: prev.documentation.options,
+                        description: stateHome.documentation.options[1],
+                      },
+                    };
+                  })
+                }
+                className={`w-1/2 sm:w-1/3 h-20 border-2 rounded-xl ${
+                  context?.darkMode
+                    ? "bg-green-200 hover:bg-green-300"
+                    : "bg-green-300 hover:bg-green-400"
+                }`}
+              >
+                {stateHome.btnDocumentation[1]}
               </button>
             </div>
             <div className="flex justify-center h-1/2 sm:h-1/3 gap-x-3 text-slate-900 font-extrabold">
               <button
-                onClick={() => dispatch({ type: "toggleDoc", payload: "why" })}
-                className={`w-1/2 sm:w-1/3 h-20 border-2 rounded-xl ${
-                  context?.darkMode
-                    ? "bg-green-200 hover:bg-green-300"
-                    : "bg-green-300 hover:bg-green-400"
-                }`}
-              >
-                {context?.language == "ENG" ? "Why do it?" : "Perchè farlo?"}
-              </button>
-              <button
                 onClick={() =>
-                  dispatch({ type: "toggleDoc", payload: "useful" })
+                  setStateHome((prev: StateHome) => {
+                    return {
+                      ...prev,
+                      toggleBtnDocumentation: !prev.toggleBtnDocumentation,
+                      documentation: {
+                        options: prev.documentation.options,
+                        description: stateHome.documentation.options[2],
+                      },
+                    };
+                  })
                 }
                 className={`w-1/2 sm:w-1/3 h-20 border-2 rounded-xl ${
                   context?.darkMode
@@ -101,9 +127,28 @@ const Home: React.FC = () => {
                     : "bg-green-300 hover:bg-green-400"
                 }`}
               >
-                {context?.language == "ENG"
-                  ? "Why is it useful?"
-                  : "Perchè è utile?"}
+                {stateHome.btnDocumentation[2]}
+              </button>
+              <button
+                onClick={() =>
+                  setStateHome((prev: StateHome) => {
+                    return {
+                      ...prev,
+                      toggleBtnDocumentation: !prev.toggleBtnDocumentation,
+                      documentation: {
+                        options: prev.documentation.options,
+                        description: stateHome.documentation.options[3],
+                      },
+                    };
+                  })
+                }
+                className={`w-1/2 sm:w-1/3 h-20 border-2 rounded-xl ${
+                  context?.darkMode
+                    ? "bg-green-200 hover:bg-green-300"
+                    : "bg-green-300 hover:bg-green-400"
+                }`}
+              >
+                {stateHome.btnDocumentation[0]}
               </button>
             </div>
           </>
--- src/components/questionnaire/Questionnaire.tsx
+++ src/components/questionnaire/Questionnaire.tsx
@@ -1,22 +1,27 @@
-import React, { useContext } from 'react'
-import { Question } from '..'
-import { DispatchContext, StarterContext } from '../../context/context';
-import { IoIosArrowBack } from 'react-icons/io';
+import React, { useContext, useEffect, useState } from "react";
+import { IoIosArrowBack } from "react-icons/io";
+import { Question } from "..";
+import { StateQuestionnaire } from "../../datatypes";
+import { QUESTIONNAIRE_ENG, QUESTIONNAIRE_ITA } from "../../data";
+import { CONTEXT, DISPATCH } from "../../context";
 
-const Questionnaire:React.FC = () => {
+const Questionnaire: React.FC = () => {
 
-  const context = useContext(StarterContext);
-  const dispatch = useContext(DispatchContext);
-  
+  const context = useContext(CONTEXT);
+  const dispatch = useContext(DISPATCH);
 
+  const [stateQuestionnaire, setStateQuestionnaire] = useState<StateQuestionnaire>(QUESTIONNAIRE_ENG);
+
+  useEffect(() => {
+    context.language == "ENG" ? setStateQuestionnaire(QUESTIONNAIRE_ENG) : setStateQuestionnaire(QUESTIONNAIRE_ITA);
+  }, [context.language])
 
   return (
     <>
-      <div className="flex items-center justify-between w-[100%] text-4xl sm:text-4xl mt-14 font-extrabold">{context?.language == "ENG"
-        ? "Question"
-        : "Domanda"} N° 1
+      <div className="flex justify-around w-[100%] text-4xl mt-10 font-extrabold">
+        {stateQuestionnaire.title + stateQuestionnaire.counterQuestions}
         <button
-          onClick={() => dispatch({ type: "toggleDoc" })}
+          onClick={() => dispatch({ type: "toggleQuestionnaire" })}
           className={`flex items-center px-3 py-2 h-10 sm:h-auto w-auto rounded-xl tracking-wider text-base sm:text-xl text-slate-900 hover:font-extrabold  ${
           context?.darkMode
             ? " bg-green-200 hover:bg-green-300"
@@ -27,9 +32,11 @@ const Questionnaire:React.FC = () => {
           <IoIosArrowBack />
         </button>
       </div>
-      <Question/>
+      <div>
+        <Question {...stateQuestionnaire.question}/>
+      </div>  
     </>
-  )
-}
+  );
+};
 
-export default Questionnaire
+export default Questionnaire;
