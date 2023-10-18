7afbafeb86f8db4821e1c911676814e4e742f6c8
David92p <Dave.panetta92@gmail.com>
2023-10-18 21:38:12 +0200

    Fixed Question component

--- src/components/questionnaire/Question.tsx
+++ src/components/questionnaire/Question.tsx
@@ -1,43 +1,43 @@
-import React, { useContext } from 'react'
-import { DispatchContext, StarterContext } from '../../context/context';
-import { IoIosArrowBack } from 'react-icons/io';
+import React, { useContext } from "react";
 
-const Question:React.FC = () => {
+import type {Question} from "../../datatypes"
+import { CONTEXT, DISPATCH } from "../../context";
 
-  const context = useContext(StarterContext);
-	const dispatch = useContext(DispatchContext);
 
-	console.log(context);
-	
+const Question: React.FC<Question> = ({ question, options, answer }) => {
 
-	return (
-		<div className={`flex flex-col ${
-			context?.darkMode
-				? "bg-green-200"
-				: "bg-green-300"
-		} text-slate-900 font-bold tracking-wider text-justify text-xl leading-loose my-8 p-4 gap-y-8 rounded-xl`}>
-				<div className='font-extrabold text-2xl sm:text-4xl text-justify leading-relaxed p-8'>
-					I surrogati della carne come i burger vegetali che hanno la pretesa di inserirsi come sostituti della carne per colore, forma e consistenza, hanno un impatto sostenibile?
-				</div>
+  const context = useContext(CONTEXT);
+  // const dispatch = useContext(DISPATCH);
 
-				<div className='flex items-start gap-4 h-auto'>
-					<input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
-					<label htmlFor="vehicle1">Si, la produzione di questi prodotti non hanno impatto sul nostro pianeta</label>
-				</div>
-				<div className='flex items-start gap-4 h-auto'>
-					<input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
-					<label htmlFor="vehicle1">No, la produzione di questi alimenti hanno un impatto superiore rispetto al consumo di carni bianche</label>
-				</div>
-				<div className='flex items-start gap-4 h-auto'>
-					<input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
-					<label htmlFor="vehicle1">No, vi sono all'interno oli raffinali parzialmente idrogenati, amido modificato, aromi chimici e glicerina che aiutano la consistenza a essere il più vicina possibile a quella della carne</label>
-				</div>
-				<div className='flex items-start gap-4 h-auto'>
-					<input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
-					<label htmlFor="vehicle1">Si, la produzione di questi alimenti permette di non utilizzare le risorse ambientali</label>
-				</div>
+ 
+  return (
+    <div className={`flex flex-col  text-slate-900 font-bold tracking-wider text-justify text-xl leading-loose my-8 p-4 gap-y-8 rounded-xl  ${
+      context?.darkMode
+        ? "bg-green-200"
+        : "bg-green-300"
+    }`}>
+			<div className='font-extrabold text-2xl sm:text-4xl text-justify leading-relaxed p-8'>
+				{ question }
+			</div>
+
+			<div className='flex items-start gap-4 h-auto'>
+				<input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
+				<label htmlFor="vehicle1">{options[0]}</label>
+			</div>
+			<div className='flex items-start gap-4 h-auto'>
+				<input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
+				<label htmlFor="vehicle1">{options[1]}</label>
+			</div>
+			<div className='flex items-start gap-4 h-auto'>
+				<input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
+				<label htmlFor="vehicle1">{options[2]}</label>
+			</div>
+			<div className='flex items-start gap-4 h-auto'>
+				<input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
+				<label htmlFor="vehicle1">{options[3]}</label>
+			</div>
 		</div>
-	)
-}
+  );
+};
 
-export default Question
+export default Question;
