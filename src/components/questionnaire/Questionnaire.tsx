79ed40530edf97b83ac2913eabeb2cd0df49f1ff
David92p <dave.panetta92@gmail.com>
2023-10-19 15:02:08 +0200

    FIxed classes color

--- src/components/questionnaire/Questionnaire.tsx
+++ src/components/questionnaire/Questionnaire.tsx
@@ -6,15 +6,17 @@ import { QUESTIONNAIRE_ENG, QUESTIONNAIRE_ITA } from "../../data";
 import { CONTEXT, DISPATCH } from "../../context";
 
 const Questionnaire: React.FC = () => {
-
   const context = useContext(CONTEXT);
   const dispatch = useContext(DISPATCH);
 
-  const [stateQuestionnaire, setStateQuestionnaire] = useState<StateQuestionnaire>(QUESTIONNAIRE_ENG);
+  const [stateQuestionnaire, setStateQuestionnaire] =
+    useState<StateQuestionnaire>(QUESTIONNAIRE_ENG);
 
   useEffect(() => {
-    context.language == "ENG" ? setStateQuestionnaire(QUESTIONNAIRE_ENG) : setStateQuestionnaire(QUESTIONNAIRE_ITA);
-  }, [context.language])
+    context.language == "ENG"
+      ? setStateQuestionnaire(QUESTIONNAIRE_ENG)
+      : setStateQuestionnaire(QUESTIONNAIRE_ITA);
+  }, [context.language]);
 
   return (
     <>
@@ -23,9 +25,9 @@ const Questionnaire: React.FC = () => {
         <button
           onClick={() => dispatch({ type: "toggleQuestionnaire" })}
           className={`flex items-center px-3 py-2 h-10 sm:h-auto w-auto rounded-xl tracking-wider text-base sm:text-xl text-slate-900 hover:font-extrabold  ${
-          context?.darkMode
-            ? " bg-green-200 hover:bg-green-300"
-            : " bg-green-400 hover:bg-green-500"
+            context?.darkMode
+              ? " bg-green-300 hover:bg-green-400"
+              : " bg-green-300 hover:bg-green-400"
           }`}
         >
           Home
@@ -33,8 +35,8 @@ const Questionnaire: React.FC = () => {
         </button>
       </div>
       <div>
-        <Question {...stateQuestionnaire.question}/>
-      </div>  
+        <Question {...stateQuestionnaire.question} />
+      </div>
     </>
   );
 };
