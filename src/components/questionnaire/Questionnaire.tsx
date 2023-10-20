3fbff400b860af95b43dc9e59c88f61129b6fd84
David92p <dave.panetta92@gmail.com>
2023-10-20 16:49:44 +0200

    Added btn next prev

--- src/components/questionnaire/Questionnaire.tsx
+++ src/components/questionnaire/Questionnaire.tsx
@@ -53,13 +53,31 @@ const Questionnaire: React.FC = () => {
           <IoIosArrowBack />
         </button>
       </div>
-      <div>
-        {isLoading ? (
-          <Loading />
-        ) : (
-          <Question {...stateQuestionnaire.questions[indexCasual[0]]} />
-        )}
-      </div>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Question {...stateQuestionnaire.questions[indexCasual[0]]} />
+      )}
+      {/* <div className="flex justify-around w-[100%] h-[100%] text-4xl mt-10 font-extrabold border-2 border-red-400">
+        <button
+          className={`flex items-center px-3 py-2 h-10 sm:h-auto w-auto rounded-xl tracking-wider text-base sm:text-xl text-slate-900 hover:font-extrabold  ${
+            context?.darkMode
+              ? " bg-green-300 hover:bg-green-400"
+              : " bg-green-300 hover:bg-green-400"
+          }`}
+        >
+          PREV
+        </button>
+        <button
+          className={`flex items-center px-3 py-2 h-10 sm:h-auto w-auto rounded-xl tracking-wider text-base sm:text-xl text-slate-900 hover:font-extrabold  ${
+            context?.darkMode
+              ? " bg-green-300 hover:bg-green-400"
+              : " bg-green-300 hover:bg-green-400"
+          }`}
+        >
+          NEXT
+        </button>
+      </div> */}
     </>
   );
 };
