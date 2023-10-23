77a4736d16f70261a6c6dc9236a159b3f4fd2911
David92p <Dave.panetta92@gmail.com>
2023-10-23 21:54:26 +0200

    Fixed data

--- src/data/questionnaire/questionnaire.ts
+++ src/data/questionnaire/questionnaire.ts
@@ -5,13 +5,11 @@ import { QUESTIONS_ENG, QUESTIONS_ITA } from "../questions/questions";
 export const QUESTIONNAIRE_ITA: StateQuestionnaire = {
     title: "Domanda N° ",
     counterQuestions: 1,
-    index: 0,
     questions: QUESTIONS_ITA,
 };
 
 export const QUESTIONNAIRE_ENG: StateQuestionnaire = {
     title: "Question N° ",
     counterQuestions: 1,
-    index: 0,
     questions: QUESTIONS_ENG,
 };
