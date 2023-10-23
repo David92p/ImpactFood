2ca21dec9554b6378a0a819df82331ccaa4477a3
David92p <Dave.panetta92@gmail.com>
2023-10-23 21:54:01 +0200

    Fixed interface

--- src/datatypes/questionnaire/stateQuestionnaire.ts
+++ src/datatypes/questionnaire/stateQuestionnaire.ts
@@ -1,13 +1,10 @@
 import { QuestionType } from "..";
 
 type Title = "Domanda N° " | "Question N° ";
-type CounterQuestions = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
-type Index = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
 interface StateQuestionnaire {
   title: Title;
-  counterQuestions: CounterQuestions;
-  index: Index;
+  counterQuestions: number;
   questions: QuestionType[];
 }
 
