6104d8935544a36c5596a0280e18d9613bfa0095
David92p <dave.panetta92@gmail.com>
2023-10-25 16:56:28 +0200

    Added logic question component

--- src/components/questionnaire/Question.tsx
+++ src/components/questionnaire/Question.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import { CONTEXT } from "../../context";
 import { QuestionType } from "../../datatypes";
@@ -10,19 +10,51 @@ const Question: React.FC<QuestionType> = ({
   // handleAnswers,
 }) => {
   const context = useContext(CONTEXT);
+  //   const [checks, setChecks] = useState({
+  //     check1: false,
+  //     check2: false,
+  //     check3: false,
+  //     check4: false,
+  //   });
+
+  //   const handleAnswer = (name: string) => {
+  //     setChecks({ check1: false, check2: false, check3: false, check4: false });
+
+  //     if (name == "answer_1") {
+  //       setChecks((prev) => {
+  //         return { ...prev, check1: !prev.check1 };
+  //       });
+  //     } else if (name == "answer_2") {
+  //       setChecks((prev) => {
+  //         return { ...prev, check2: !prev.check2 };
+  //       });
+  //     } else if (name == "answer_3") {
+  //       setChecks((prev) => {
+  //         return { ...prev, check3: !prev.check3 };
+  //       });
+  //     } else if (name == "answer_4") {
+  //       setChecks((prev) => {
+  //         return { ...prev, check4: !prev.check4 };
+  //       });
+  //     }
+  //   };
+  //console.log(checks);
+
+  //   const isAnswearSelected = (e: React.ChangeEvent<HTMLInputElement>): void => {
+  //     console.log("click");
+  //   };
 
   return (
     <div
-      className={`flex flex-col tracking-wider leading-loose sm:mx-0 rounded-xl mt-3 text-justify border-2 border-red-800 ${
+      className={`flex flex-col tracking-wider leading-loose sm:mx-0 rounded-xl mt-3 mx-2 text-justify${
         alert ? "opacity-25" : "opacity-100"
       } ${context?.darkMode ? "text-slate-900" : "bg-green-300"}`}
     >
       <div
-        className={`font-extrabold text-xl leading-relaxed mx-2 py-4 px-2 mb-4 mt-2 rounded-lg bg-green-200`}
+        className={`font-extrabold text-xl leading-relaxed mx-2 py-4 px-2 mb-4 mt-2 rounded-lg bg-green-200 text-center`}
       >
         {question}
       </div>
-
       <div
         className={`flex items-center gap-2 m-4 2xl:mx-6 p-2 hover:m-2 text-md font-bold transition-all rounded-lg hover:font-extrabold cursor-pointer  ${
           context?.darkMode
@@ -32,12 +64,9 @@ const Question: React.FC<QuestionType> = ({
       >
         <input
           type="checkbox"
-          id="answer_1"
-          name="answer_1"
-          // checked= {false}
-          // value={options.indexOf(options[0])}
-          // onChange={() => handleAnswers(1)}
-          className={`h-7 w-10 2xl:w-7 hover:h-8 hover:w-12 2xl:hover:w-9 2xl:hover:h-9 ${
+          name="answer"
+          value={options.indexOf(options[0])}
+          className={`h-7 w-10 2xl:w-7 hover:h-8 hover:w-12 2xl:hover:w-9 2xl:hover:h-9 cursor-pointer ${
             context.darkMode
               ? "bg-slate-100 hover:border-2 hover:border-slate-900"
               : "bg-green-200 hover:bg-green-300 2xl:bg-green-300 hover:border-2 hover:border-slate-900"
@@ -47,7 +76,7 @@ const Question: React.FC<QuestionType> = ({
           icon={faCarrot}
           className=" apparea absolute h-6 ml-1 text-opacity-0 check-1 transition "
         /> */}
-        <label htmlFor="answer_1" className="cursor-pointer w-[100%]">
+        <label htmlFor="answer" className="cursor-pointer w-[100%]">
           {options[0]}
         </label>
       </div>
@@ -61,17 +90,15 @@ const Question: React.FC<QuestionType> = ({
       >
         <input
           type="checkbox"
-          id="answer_2"
-          name="answer_2"
-          //checked={false}
-          // value={options.indexOf(options[1])}
+          name="answer"
+          value={options.indexOf(options[1])}
           className={`h-7 w-10 2xl:w-7 hover:h-8 hover:w-12 2xl:hover:w-9 2xl:hover:h-9 cursor-pointer ${
             context.darkMode
               ? "bg-slate-100 hover:border-2 hover:border-slate-900"
               : "bg-green-200 hover:bg-green-300 2xl:bg-green-300 hover:border-2 hover:border-slate-900"
           } rounded-md transition-all`}
         />
-        <label htmlFor="answer_2" className="cursor-pointer w-[100%]">
+        <label htmlFor="answer" className="cursor-pointer w-[100%]">
           {options[1]}
         </label>
       </div>
@@ -85,17 +112,15 @@ const Question: React.FC<QuestionType> = ({
       >
         <input
           type="checkbox"
-          id="answer_3"
-          name="answer_3"
-          //checked={false}
-          // value={options.indexOf(options[2])}
+          name="answer"
+          value={options.indexOf(options[2])}
           className={`h-7 w-10 2xl:w-7 hover:h-8 hover:w-12 2xl:hover:w-9 2xl:hover:h-9 cursor-pointer ${
             context.darkMode
               ? "bg-slate-100 hover:border-2 hover:border-slate-900"
               : "bg-green-200 hover:bg-green-300 2xl:bg-green-300 hover:border-2 hover:border-slate-900"
           } rounded-md transition-all`}
         />
-        <label htmlFor="answer_3" className="cursor-pointer w-[100%]">
+        <label htmlFor="answer" className="cursor-pointer w-[100%]">
           {options[2]}
         </label>
       </div>
@@ -109,17 +134,15 @@ const Question: React.FC<QuestionType> = ({
       >
         <input
           type="checkbox"
-          id="answer_4"
-          name="answer_4"
-          //checked={false}
-          // value={options.indexOf(options[3])}
+          name="answer"
+          value={options.indexOf(options[3])}
           className={`h-7 w-10 2xl:w-7 hover:h-8 hover:w-12 2xl:hover:w-9 2xl:hover:h-9 cursor-pointer ${
             context.darkMode
               ? "bg-slate-100 hover:border-2 hover:border-slate-900"
               : "bg-green-200 hover:bg-green-300 2xl:bg-green-300 hover:border-2 hover:border-slate-900"
           } rounded-md transition-all`}
         />
-        <label htmlFor="answer_4" className="cursor-pointer w-[100%]">
+        <label htmlFor="answer" className="cursor-pointer w-[100%]">
           {options[3]}
         </label>
       </div>
