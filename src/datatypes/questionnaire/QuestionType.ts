bb4f0ad8b4e6d7b6ceaabef635ebdfc7d61bfee6
David92p <dave.panetta92@gmail.com>
2023-10-26 16:29:21 +0200

    Added fnc at question interface

--- src/datatypes/questionnaire/QuestionType.ts
+++ src/datatypes/questionnaire/QuestionType.ts
@@ -3,6 +3,7 @@ interface QuestionType {
   options: [string, string, string, string];
   answer: 0 | 1 | 2 | 3;
   alert?: boolean;
+  getDataResult?: (answer: number, result: number, question?: number) => void;
 }
 
 export default QuestionType;
