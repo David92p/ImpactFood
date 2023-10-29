b8328dc423850a046cd79b0cef6319f59fc258a9
David92p <Dave.panetta92@gmail.com>
2023-10-29 10:51:45 +0100

    Added proprierty

--- src/datatypes/questionnaire/QuestionType.ts
+++ src/datatypes/questionnaire/QuestionType.ts
@@ -2,6 +2,7 @@ interface QuestionType {
   question: string;
   options: [string, string, string, string];
   answer: 0 | 1 | 2 | 3;
+  counterQuestion?: number;
   alert?: boolean;
   getDataResult?: (answer: number, result: number, question?: number) => void;
 }
