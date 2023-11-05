04fb5c4fe44d340fb1966bf12b5e8b24e980e797
David92p <Dave.panetta92@gmail.com>
2023-11-05 16:05:04 +0100

    Added first part result

--- src/components/result/Result.tsx
+++ src/components/result/Result.tsx
@@ -1,14 +1,41 @@
 import React, { useContext } from "react";
-import { DISPATCH } from "../../context";
+import { CONTEXT, DISPATCH } from "../../context";
+import { ResultType } from "../../datatypes";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 
-const Result: React.FC = () => {
+const Result: React.FC<ResultType> = ({ corrects }) => {
+  const context = useContext(CONTEXT)
   const dispatch = useContext(DISPATCH);
 
+  //console.log(corrects);
   
-  return <div>RESULT HERE .......btn.
-    <button onClick={() => dispatch({ type: "toggleQuestionnaire" })}>BACK</button>
-  </div>;
+  return (
+    <div className="flex flex-col w-[100%] mt-14 border-2 border-red-500">
+      <div className="flex justify-center gap-6 w-[100%] h-28 bg-green-300">
+        {corrects.length >= 8 
+        ? (
+          <>
+            <FontAwesomeIcon icon={faStar} rotation={270} style={{color: "#0f172a",}} className="w-20 h-20 mt-4"/>
+            <FontAwesomeIcon icon={faStar} style={{color: "#0f172a"}} className="w-20 h-20"/>
+            <FontAwesomeIcon icon={faStar} rotation={90} style={{color: "#0f172a",}} className="w-20 h-20 mt-4"/>
+          </>
+          ) 
+        : (corrects.length >= 5 
+          ? (
+            <>
+              <FontAwesomeIcon icon={faStar} rotation={270} style={{color: "#0f172a",}} className="w-20 h-20 mt-4"/>
+              <FontAwesomeIcon icon={faStar} rotation={90} style={{color: "#0f172a",}} className="w-20 h-20 mt-4"/>
+            </>
+          )
+          : <FontAwesomeIcon icon={faStar} style={{color: "#0f172a"}} className="w-20 h-20"/>
+        )}
+        
+      </div>
+      <button onClick={() => dispatch({ type: "toggleQuestionnaire" })}>BACK</button>
+    </div>
+  )
 };
 
 export default Result;
