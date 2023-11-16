b994df6daca9622b0b00fe757aac6fce16cf9583
David92p <dave.panetta92@gmail.com>
2023-11-16 13:00:59 +0100

    Removed commented code

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,32 +1,3 @@
 import { initialState, CONTEXT, DISPATCH, Reducer } from "./context";
 
 export { initialState, CONTEXT, DISPATCH, Reducer };
-
-
-// // STARTER
-// import {
-//   initialStateStarter,
-//   StarterContext,
-//   StarterDispatchContext,
-//   starterReducer,
-// } from "./contextStarter";
-
-// // QUESTIONNAIRE
-// import {
-//   initialStateQuestionnaire,
-//   QuestionnaireContex,
-//   QuestionnaireDispatchContext,
-//   questionnaireReducer,
-// } from "./contextQuestionnaire";
-
-// export {
-//   initialStateStarter,
-//   StarterContext,
-//   StarterDispatchContext,
-//   starterReducer,
-//   initialStateQuestionnaire,
-//   QuestionnaireContex,
-//   QuestionnaireDispatchContext,
-//   questionnaireReducer,
-// };
-
