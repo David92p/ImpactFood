4ea581e7918d7ff42b9d2a3a27a888715926fc5b
David92p <dave.panetta92@gmail.com>
2023-11-16 13:01:12 +0100

    Removed commented code

--- src/datatypes/alert/alertType.ts
+++ src/datatypes/alert/alertType.ts
@@ -1,11 +1,7 @@
-// type goBack = "DO YOU WANT TO INTERRUP THE QUESTIONNAIRE?" | "VUOI INTERROMPERE IL QUESTIONARIO?";
-// type errorResult = "ANSWER ALL QUESTIONS BEFORE CONTINUING" | "RISPONDI A TUTTE LE DOMANDE PRIMA DI PROSEGUIRE";
-
 interface AlertType {
-    type: "errorResult" | "goBack"
-    // title: goBack | errorResult
-    toggle: boolean
-    data?: number[]
+  type: "errorResult" | "goBack";
+  toggle: boolean;
+  data?: number[];
 }
 
-export default AlertType
+export default AlertType;
